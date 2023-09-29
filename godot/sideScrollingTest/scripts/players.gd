ed6af5bb39a13fd87b97e7c9e2dd7994e28fe13a
spoiboi <ntbearden@gmail.com>
2023-09-29 11:09:59 -0500

    Added sprites and animation

--- godot/sideScrollingTest/scripts/players.gd
+++ godot/sideScrollingTest/scripts/players.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 300
 @export var gravity = 20
-@export var jump_force = 300
+@export var jump_force = 500
 
 func _physics_process(delta):
 	if !is_on_floor():
@@ -11,9 +11,17 @@ func _physics_process(delta):
 			velocity.y = 500
 
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_force
+		if is_on_floor():
+			velocity.y = -jump_force
 	
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 	velocity.x = speed * horizontal_direction
+	if velocity.x != 0:
+		if velocity.x > 0:
+			$PlayerAnimation.play("walk")
+		else:
+			$PlayerAnimation.play("walk_back")
+	if horizontal_direction == 0 or !is_on_floor():
+		$PlayerAnimation.stop()
 	move_and_slide()
 
