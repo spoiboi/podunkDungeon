295a9c34bd7875b645ae737bb5fe9a34d345fe3b
spoiboi <ntbearden@gmail.com>
2023-10-22 18:59:17 -0500

    created Player2 and DummyObject

--- godot/sideScrollingTest/scripts/players.gd
+++ godot/sideScrollingTest/scripts/players.gd
@@ -3,25 +3,74 @@ extends CharacterBody2D
 @export var speed = 300
 @export var gravity = 20
 @export var jump_force = 500
+var isForward = true
+
 
 func _physics_process(delta):
+	##applies gravity
 	if !is_on_floor():
 		velocity.y += gravity
 		if velocity.y > 500:
 			velocity.y = 500
 
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = -jump_force
 	
+	##gets direction of player
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	
+	if horizontal_direction == -1:
+		isForward = false
+	elif horizontal_direction == 1:
+		isForward = true
+	
+	##applies movement
 	velocity.x = speed * horizontal_direction
-	if velocity.x != 0:
-		if velocity.x > 0:
+	
+	##animates on floor
+	if velocity.x != 0 and is_on_floor():
+		if isForward:
 			$PlayerAnimation.play("walk")
 		else:
 			$PlayerAnimation.play("walk_back")
-	if horizontal_direction == 0 or !is_on_floor():
+			
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+#		if isForward:
+#			$PlayerAnimation.play("jumping")
+#		else:
+#			$PlayerAnimation.play("jumping_back")
+	##animates in air	
+	if !is_on_floor():
+		if isForward:
+			$PlayerAnimation.play("jumping")
+		else:
+			$PlayerAnimation.play("jumping_back")
+			
+			
+	
+			
+	##stops animation		
+	if velocity.x==0 and is_on_floor():
+		if isForward:
+			$PlayerAnimation.play("walk")
+		else:
+			$PlayerAnimation.play("walk_back")
 		$PlayerAnimation.stop()
+		
+		
+	##Attacking
+#	if Input.is_action_just_pressed("attack"):
+#		if isForward:
+#			$PlayerAnimation.play("attack")
+#			$AttackAnimation.play("attack")
+#		else:
+#			$PlayerAnimation.play("attack_back")
+#			$AttackAnimationBack.play("attack")
+#			for
+#		$AttackAnimation.play("blank")
+#		$AttackAnimationBack.play("blank")
+#		$AttackAnimation.stop()
+#		$AttackAnimationBack.stop()
+#		$PlayerAnimation.stop()
+	##updates screen	
 	move_and_slide()
 
